cdab4986c3ee5d9d5e75f5c2314aa8f9012c643d
EduardoSantana446 <124050867@upq.edu.mx>
2025-11-02 16:55:23 -0600

    Se agrego diseño para la parte superior y el logo

--- AhorraMas/screens/01-inicioDeSesion.js
+++ AhorraMas/screens/01-inicioDeSesion.js
@@ -1,26 +1,57 @@
 import { Text, StyleSheet, View, TextInput, Image, ActivityIndicator, Dimensions } from 'react-native'
 import React, { useState, useEffect } from 'react'
-const {height} = Dimensions.get('window');
+const {width, height} = Dimensions.get('window');
 
 
 // Funcion Pantalla de carga
 
 const PantallaInicial = () => {
   return (
-    <View style={styles.pantallaInicialContainer}>
+    <View style={stylesIntro.pantallaInicialContainer}>
 
       <Image
         source={require("../assets/logoAhorraMas.png")}
-        style={styles.logo}
+        style={stylesIntro.logo}
         
       />
 
-      <Text style={styles.nombreApp}>AHORRA+APP</Text>
+      <Text style={stylesIntro.nombreApp}>AHORRA+APP</Text>
       <ActivityIndicator size="large" color="white" style={{marginTop: 40}}/>
 
     </View>
   );
 };
+
+
+// Funcion Inicio de sesion
+
+const Login = () => {
+
+  return (
+    <View style={stylesLogin.containerLogin}>
+
+      <View style={stylesLogin.disenoSuperior}/>
+
+      <View style={stylesLogin.logoContainer}>
+
+        <Image
+        source={require("../assets/logoAhorraMas.png")}
+        style={stylesLogin.logo}
+        />
+
+        
+
+      </View>
+
+      <Text style={stylesLogin.titulo}>Iniciar sesión</Text>
+    </View>
+
+    
+  );
+
+}
+
+
 
 
 
@@ -29,7 +60,6 @@ export default function InicioDeSesion() {
   const [inicioApp, setInicioApp] = useState(true);
 
   useEffect( () => {
-    console.log("Pantalla");
     const tiempo = setTimeout(() => {
       setInicioApp(false);
     }, 3000);
@@ -42,16 +72,14 @@ export default function InicioDeSesion() {
     return <PantallaInicial/>;
   } else {
     return (
-      <View>
-        <Text style={styles.texto}>Hola mundo</Text>
-      </View>
+      <Login/>
     )
   }
 
    
 }
 
-const styles = StyleSheet.create({
+const stylesIntro = StyleSheet.create({
 
   pantallaInicialContainer: {
     flex: 1,
@@ -73,17 +101,47 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
 
-  loadingText: {
-    color: 'white',
-    marginTop: 10,
-    fontSize: 16,
+});
+
+const stylesLogin = StyleSheet.create({
+
+  containerLogin: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    paddingHorizontal: 20,
   },
 
-  texto: {
-    fontSize: 20,
-    color: 'black',
-    textAlign: 'center',
-    marginTop: 20,
+  disenoSuperior: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: width * 1.5,
+    height: 180,
+    backgroundColor: 'green',
+    borderBottomLeftRadius: width * 0.8,
+    zIndex: 1,
   },
 
+  logoContainer: {
+    alignItems: 'center',
+    marginTop: 50,
+    zIndex: 10,
+  },
+
+  logo:{
+    width: 60,
+    height: 60,
+    resizeMode: 'contain',
+  },
+
+  titulo: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 30,
+    alignSelf: 'flex-start',
+    color: '#000000ff',
+    zIndex: 10,
+    fontFamily: 'Montserrat',
+  }
 });
