79d47cd7a5be15656e5d26649c208abeac6643a6
marianagarciaolvera19 <marianagarciaolvera19@gmail.com>
2025-11-04 17:20:30 -0600

    v1 terminada

---
+++ AhorraMas/screens/03-graficasScreen.js
@@ -0,0 +1,10 @@
+import { Text, StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button } from 'react-native-web'
+
+export default function GraficasScreen() {
+
+}
+
+
+const styles = StyleSheet.create({});
